bef21fa6666e43d0ee17df70331e8047f0fde7bb
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-04-28 18:56:46 +0200

    zmodyfikowane help

--- skrypt.sh
+++ skrypt.sh
@@ -3,6 +3,8 @@
 if [[ $1 == "--help" ]]; then
     echo "Mozliwe opcje:"
     echo "--date -d #wyswietla aktualna date"
-    echo "--logs -l <liczba>  (bez podania liczby tworzy 100)"
+    echo "--logs -l <liczba>  (bez podania liczby - 100)"
+    echo "--error -e <liczba> (bez podawania liczby - 100)"
+    echo "--init #sklonowanie repozytorium"
     echo "--help -h #wyswietla pomoc"
 fi
