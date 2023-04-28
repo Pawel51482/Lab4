755bd72c0d9699d35beca090646e54aec7f7b1bf
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-04-28 17:15:14 +0200

    pomoc

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == "--logs" && "$2" =~ ^[0-9]+$ ]]; then
-    for i in $(seq 1 $2); do
-        touch log${i}.txt
-        echo "Nazwa pliku: log${i}.txt" >> log${i}.txt
-        echo "Nazwa skryptu: skrypt.sh" >> log${i}.txt
-        echo "Data utworzenia: $(date)" >> log${i}.txt
-    done
+if [[ $1 == "--help" ]]; then
+    echo "Mozliwe opcje:"
+    echo "--date #wyswietla aktualna date"
+    echo "--logs <liczba>  (bez podania liczby tworzy 100)"
+    echo "--help #wysweitla pomoc"
 fi
