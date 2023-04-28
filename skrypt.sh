7209a7906d55ba98db1dc329b293f47f29330995
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-04-28 18:52:03 +0200

    Errory

--- skrypt.sh
+++ skrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$1" == "--error" && "$2" =~ ^[0-9]+$ ]]; then
+if [[ "$1" == "--error" && "$2" =~ ^[0-9]+$ ]] || [[ "$1" == "-e" && "$2" =~ ^[0-9]+$ ]]; then
     for i in $(seq 1 $2); do
         mkdir -p error${i}
         touch error${i}/error${i}.txt
@@ -8,23 +8,7 @@ if [[ "$1" == "--error" && "$2" =~ ^[0-9]+$ ]]; then
         echo "Nazwa skryptu: skrypt.sh" >> error${i}.txt
         echo "Data utworzenia: $(date)" >> error${i}.txt
     done
-elif [[ "$1" == "--error" ]]; then
-    for i in {1..100}
-    do
-      filename="error$i.txt"
-      echo "Nazwa pliku: $filename" >> $filename
-      echo "Nazwa skryptu: skrypt.sh" >> $filename
-      echo "Data: $(date)" >> $filename
-    done
-elif [[ "$1" == "-e" && "$2" =~ ^[0-9]+$ ]]; then
-    for i in $(seq 1 $2); do
-        mkdir -p error${i}
-        touch error${i}/error${i}.txt
-        echo "Nazwa pliku: error${i}.txt" >> error${i}.txt
-        echo "Nazwa skryptu: skrypt.sh" >> error${i}.txt
-        echo "Data utworzenia: $(date)" >> error${i}.txt
-    done
-elif [[ "$1" == "-e" ]]; then
+elif [[ "$1" == "--error" ]] || [[ "$1" == "-e" ]]; then
     for i in {1..100}
     do
       filename="error$i.txt"
