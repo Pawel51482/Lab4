42bb261ca6b6a6e2b209cd2f0d6cb55491c34e4d
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-04-28 18:30:19 +0200

    errory

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,35 @@
-if [[ "$1" == "--init" ]]; then
-    git clone https://github.com/Pawel51482/Lab4.git
-    echo "export PATH=\"\${PATH}:$(pwd)/Lab4\"" >> ~/.bashrc
+#!/bin/bash
+
+if [[ "$1" == "--error" && "$2" =~ ^[0-9]+$ ]]; then
+    for i in $(seq 1 $2); do
+        mkdir -p error${i}
+        touch error${i}/error${i}.txt
+        echo "Nazwa pliku: log${i}.txt" >> log${i}.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log${i}.txt
+        echo "Data utworzenia: $(date)" >> log${i}.txt
+    done
+elif [[ "$1" == "--error" ]]; then
+    for i in {1..100}
+    do
+      filename="error$i.txt"
+      echo "Nazwa pliku: $filename" >> $filename
+      echo "Nazwa skryptu: skrypt.sh" >> $filename
+      echo "Data: $(date)" >> $filename
+    done
+elif [[ "$1" == "-e" && "$2" =~ ^[0-9]+$ ]]; then
+    for i in $(seq 1 $2); do
+        mkdir -p error${i}
+        touch error${i}/error${i}.txt
+        echo "Nazwa pliku: log${i}.txt" >> log${i}.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log${i}.txt
+        echo "Data utworzenia: $(date)" >> log${i}.txt
+    done
+elif [[ "$1" == "-e" ]]; then
+    for i in {1..100}
+    do
+      filename="error$i.txt"
+      echo "Nazwa pliku: $filename" >> $filename
+      echo "Nazwa skryptu: skrypt.sh" >> $filename
+      echo "Data: $(date)" >> $filename
+    done
 fi
